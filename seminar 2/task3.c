de7033125688507e12e269da8e9fb525f65034e0
jakka <kuznetssovvzh@gmail.com>
2022-09-15 18:11:51 +0300

    optimized task 3 in 2nd seminar hw

--- seminar 2/task3.c
+++ seminar 2/task3.c
@@ -9,17 +9,28 @@ int main()
     for (int i = 0; i < n; ++i)
         scanf("%i", &a[i]);
 
-    int flag = 1;
-    for (int i = 0; i < n; ++i) {
-        if (!(i == 0)) {
-            if (a[i] < a[i - 1]) {
-                printf("%i\n", i - 1);
-                flag = 0;
-                break;
-            }
+    int left = 0, right = n - 1;
+    while (right > left + 1)
+    {
+        int mid1 = (left + (right - left + 1) / 3);
+        int mid2 = (left + (2 * (right - left) - 1) / 3);
+        if (a[mid1] == a[mid2] && mid1 != mid2)
+        {
+            right = mid2;
+            left = mid1;
         }
-
+        else if (mid1 == mid2)
+        {
+            mid2 = right - 1;
+            mid1 = left + 1;
+        }
+        if (a[mid1] > a[mid2])
+            right = mid2;
+        else
+            left = mid1;
     }
-    if (flag)
-        printf("%i\n", n - 1);
+    if (a[right] >= a[left])
+    printf("%i\n", right);
+    else
+    printf("%i\n", left);
 }
